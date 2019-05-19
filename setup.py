f9ad38410b23d10c59624a5f3236eb6eb9a045dd
vilst3r <clifford.phan@gmail.com>
2019-05-20 05:38:54 +1000

    Add installation for iterm2 & homebrew

--- setup.py
+++ setup.py
@@ -6,12 +6,37 @@ Script to automate setup of unix environment with personal configurations and to
 
 import subprocess
 
-def install_iterm2():
-    command = 'ls -la'
-    output = subprocess.check_output(command.split()).decode('utf-8')
-    print(output)
+def install_homebrew() -> int:
+    command = '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    while True:
+        output = process.stdout.readline()
+
+        if output:
+            print(output.strip().decode('utf-8'))
+        else:
+            break
+    rc = process.poll()
+    return rc
+
+def install_iterm2() -> int:
+    command = 'brew cask install iterm2'
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    while True:
+        output = process.stdout.readline()
+
+        if output:
+            print(output.strip().decode('utf-8'))
+        else:
+            break
+    rc = process.poll()
+    return rc
 
 if __name__ == '__main__':
-    install_iterm2()
-    print('hello')
+    #if install_homebrew() != 0:
+    #   raise Exception('Failed to install homebrew')
+    if install_iterm2() != 0:
+       raise Exception('Failed to install iterm2')
 
