5cb63d4ceb9222dd47d5652f8dd650001038ed45
vilst3r <clifford.phan@gmail.com>
2019-05-21 11:09:20 +1000

    Refactor process output into single method

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ Script to automate setup of unix environment with personal configurations and to
 
 import subprocess
 
-def install_homebrew() -> int:
-    command = '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+def process_realtime_output(process: subprocess.Popen) -> int:
+    '''
+    Output child stdout in root process call
+    '''
+    if not process:
+        return 1
 
     while True:
         output = process.stdout.readline()
@@ -19,24 +22,41 @@ def install_homebrew() -> int:
             break
     rc = process.poll()
     return rc
+
+#def config_vim() -> int:
+#    return
+
+def install_homebrew() -> int:
+    '''
+    Install homebrew if it doesn't exist in *nix environment
+    '''
+
+    command = '/usr/bin/ruby -e'.split()
+    command.append("$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)") 
+    print(command)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    return process_realtime_output(process) 
 
 def install_iterm2() -> int:
+    '''
+    Install iterm2 through cask
+    '''
+
     command = 'brew cask install iterm2'
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return process_realtime_output(process) 
 
-    while True:
-        output = process.stdout.readline()
+def install_vim_configs() -> int:
+    '''
+    Configure vim settings including allocated theme
+    '''
 
-        if output:
-            print(output.strip().decode('utf-8'))
-        else:
-            break
-    rc = process.poll()
-    return rc
-
+    return
+    
 if __name__ == '__main__':
-    #if install_homebrew() != 0:
-    #   raise Exception('Failed to install homebrew')
+#    if install_homebrew() != 0:
+#       raise Exception('Failed to install homebrew')
     if install_iterm2() != 0:
        raise Exception('Failed to install iterm2')
 
