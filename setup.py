34b238532d3a15a6174384672930a92a4518f636
vilst3r <clifford.phan@gmail.com>
2020-03-01 21:50:46 +1100

    Move logger to own module

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-'''
+"""
 Script to automate setup of unix environment with personal configurations and tools
-'''
+"""
 
 # Native Modules
-import logging
 import time
 
 # Custom Modules
+from utils.logger import LOGGER, initialise_logger
 from utils.setup_wrapper import SETUP
 from utils.github_wrapper import GITHUB
 import utils.powerline_helper as powerline_helper
@@ -18,52 +18,54 @@ import utils.brew_helper as brew_helper
 import utils.vim_helper as vim_helper
 import utils.bash_helper as bash_helper
 
-LOGGER = logging.getLogger()
 
 def install_brew_packages():
-    '''
-    Install brew packages
-    '''
-    SETUP.print_process_step_start('Installing brew packages...')
+    """
+	Install brew packages
+	"""
+    SETUP.print_process_step_start("Installing brew packages...")
 
     brew_helper.install_all_brew_packages()
 
-    SETUP.print_process_step_finish('Installation of brew packages are complete!')
+    SETUP.print_process_step_finish("Installation of brew packages are complete!")
+
 
 def install_cask_packages():
-    '''
-    Reads brew-cask.txt file in child config directory to install all software applications
-    '''
-    SETUP.print_process_step_start('Installing cask packages...')
+    """
+	Reads brew-cask.txt file in child config directory to install all software applications
+	"""
+    SETUP.print_process_step_start("Installing cask packages...")
 
     brew_helper.install_all_cask_packages()
 
-    SETUP.print_process_step_finish('Installation of cask packages are complete!')
+    SETUP.print_process_step_finish("Installation of cask packages are complete!")
+
 
 def install_homebrew():
-    '''
-    Install homebrew & cask if it doesn't exist in *nix environment and requires password input
-    '''
-    SETUP.print_process_step_start('Installing homebrew...')
+    """
+	Install homebrew & cask if it doesn't exist in *nix environment and requires password input
+	"""
+    SETUP.print_process_step_start("Installing homebrew...")
 
-    if brew_helper.brew_exists():
-        SETUP.print_process_step_finish('Homebrew is already installed!')
-        return
+    # if brew_helper.brew_exists():
+    # 	SETUP.print_process_step_finish("Homebrew is already installed!")
+    # 	return
 
     brew_helper.install_brew()
     brew_helper.tap_brew_cask()
 
-    SETUP.print_process_step_finish('Installation of homebrew is complete!')
+    SETUP.print_process_step_finish("Installation of homebrew is complete!")
+
 
 def configure_git_ssh():
-    '''
-    Configure git ssh key to user ssh agent
-    '''
-    SETUP.print_process_step_start('Configuring Git SSH...')
+    """
+	Configure git ssh key to user ssh agent
+	"""
+    SETUP.print_process_step_start("Configuring Git SSH...")
 
-    if ssh_helper.public_key_exists() and git_helper.public_key_exists_on_github():
-        SETUP.print_process_step_finish('Git SSH is already configured!')
-        return
+    # if ssh_helper.public_key_exists() and git_helper.public_key_exists_on_github():
+    # 	SETUP.print_process_step_finish("Git SSH is already configured!")
+    # 	return
 
     ssh_helper.generate_rsa_keypair()
     ssh_helper.start_ssh_agent()
@@ -73,40 +75,43 @@ def configure_git_ssh():
     current_public_key = ssh_helper.get_public_key()
 
     payload = {}
-    payload['title'] = 'script-env-pub-key'
-    payload['key'] = current_public_key
+    payload["title"] = "script-env-pub-key"
+    payload["key"] = current_public_key
     GITHUB.create_public_key(payload)
 
-    SETUP.print_process_step_finish('SSH key for Git is now configured!')
+    SETUP.print_process_step_finish("SSH key for Git is now configured!")
+
 
 def configure_vim():
-    '''
-    Configure vim settings
-    '''
-    SETUP.print_process_step_start('Configuring vim...')
+    """
+	Configure vim settings
+	"""
+    SETUP.print_process_step_start("Configuring vim...")
 
     vim_helper.pull_vim_settings()
     vim_helper.configure_vimrc()
     vim_helper.configure_color_themes()
 
-    SETUP.print_process_step_finish('Vim is now configured!')
+    SETUP.print_process_step_finish("Vim is now configured!")
+
 
 def configure_bash():
-    '''
-    Configure bash settings
-    '''
-    SETUP.print_process_step_start('Configuring bash...')
+    """
+	Configure bash settings
+	"""
+    SETUP.print_process_step_start("Configuring bash...")
 
     bash_helper.pull_bash_settings()
     bash_helper.configure_bash_profile()
 
-    SETUP.print_process_step_finish('Bash is now configured!')
+    SETUP.print_process_step_finish("Bash is now configured!")
+
 
 def install_powerline():
-    '''
-    Install powerline & configure it to bash & vim
-    '''
-    SETUP.print_process_step_start('Installing powerline...')
+    """
+	Install powerline & configure it to bash & vim
+	"""
+    SETUP.print_process_step_start("Installing powerline...")
 
     powerline_helper.install_powerline_at_user()
     powerline_helper.write_bash_daemon()
@@ -118,55 +123,27 @@ def install_powerline():
     powerline_helper.config_git_colorscheme()
     powerline_helper.config_git_shell()
 
-    SETUP.print_process_step_finish('Powerline is installed & configured!')
+    SETUP.print_process_step_finish("Powerline is installed & configured!")
 
-def pretty_print_wrapper(wrapper: object, title: str):
-    '''
-    Function to pretty print wrapper in beginning of setup
-    '''
-    LOGGER.debug(f'###### {title} #####')
-    LOGGER.debug(f'{wrapper}')
 
-def initialise_logger():
-    '''
-    Set up logging for writing stdout & stderr to files
-    '''
-    LOGGER.setLevel(logging.DEBUG)
-
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-    out_path = 'logs/setup_out.log'
-    err_path = 'logs/setup_err.log'
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
-
-    out_handler = logging.FileHandler(f'{out_path}', 'w+')
-    out_handler.setLevel(logging.DEBUG)
-    out_handler.setFormatter(formatter)
-
-    err_handler = logging.FileHandler(f'{err_path}', 'w+')
-    err_handler.setLevel(logging.WARNING)
-    err_handler.setFormatter(formatter)
-
-    LOGGER.addHandler(out_handler)
-    LOGGER.addHandler(err_handler)
-    LOGGER.addHandler(stream_handler)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     initialise_logger()
 
-    START = time.time()
-    pretty_print_wrapper(SETUP, 'SetupWrapper')
-    pretty_print_wrapper(GITHUB, 'GithubWrapper')
+    LOGGER.debug("###### SetupWrapper #####")
+    LOGGER.debug(SETUP)
+    LOGGER.debug("###### GithubWrapper #####")
+    LOGGER.debug(GITHUB)
 
-    configure_git_ssh()
-    install_homebrew()
-    install_brew_packages()
-    install_cask_packages()
-    configure_vim()
-    configure_bash()
-    install_powerline()
+    START = time.time()
+
+    # configure_git_ssh()
+    # install_homebrew()
+    # install_brew_packages()
+    # install_cask_packages()
+    # configure_vim()
+    # configure_bash()
+    # install_powerline()
 
     END = time.time()
-    LOGGER.info(f'Setup time: {END - START} seconds')
+    TIME_ELAPSED = END - START
+    LOGGER.info(f"Setup time: {TIME_ELAPSED} seconds")
